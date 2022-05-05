60e469cc35a63bfa1ef1bbce9c2d013fc6e5f174
Martino888 <maxmartino33@gmail.com>
2022-05-05 23:24:12 +0200

    non va iterazione foreach

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,28 @@
     <title>home</title>
 </head>
 <body>
-@dd($listaTreni)
+    <div class="container">
+        <ul>
+            <li>Azienda</li>
+            <li>Stazione di partenza</li>
+            <li>Stazione di arrivo</li>
+            <li>Orario di partenza</li>
+            <li>Orario di arrivo</li>
+            <li>Codice treno</li>
+            <li>Numero carrozza</li>
+        </ul>
+
+        @foreach ($listaTreni as $train)
+        <div>
+            {{ $train->Azienda }}
+            {{ $train->Stazione_di_partenza }}
+            {{ $train->Stazione_di_arrivo }}
+            {{ $train->Orario_di_partenza }}
+            {{ $train->Orario_di_arrivo }}
+            {{ $train->Codice_treno }}
+            {{ $train->Numero_carrozza }}
+        </div>
+        @endforeach
+    </div>
 </body>
 </html>
